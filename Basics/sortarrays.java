be13886ecba78a01b1ace1b7d3b0245e44c9e469
AKARSH JHA <129477999+Akarshjha03@users.noreply.github.com>
2025-05-12 22:01:22 +0530

    Create sortarrays.java

---
+++ Basics/sortarrays.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        int arr[] = {2,1,4,5,3};
+        
+        Arrays.sort(arr);
+        
+        System.out.println(arr);
+    }
+}
